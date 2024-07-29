3dd95f178aea31bcd05d75d01aa4a17393d30da1
DarkMerguez <jlaroche.69008@gmail.com>
2024-07-29 16:17:55 +0200

    Codage d'un carousel dans le component home et ajout d'un component dashboard

--- app-stock/src/app/app.routes.ts
+++ app-stock/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
 import { SigninComponent } from './components/signin/signin.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path : "signup",component:SignupComponent},
-    {path : "signin",component:SigninComponent}
+    {path : "signin",component:SigninComponent},
+    {path : "home",component:HomeComponent},
+    {path : "dashboard",component:DashboardComponent}
 ];
