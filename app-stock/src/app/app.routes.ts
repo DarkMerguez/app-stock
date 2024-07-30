07b02d84927ec4b6116d061cca5ba6edf1ab887f
DarkMerguez <jlaroche.69008@gmail.com>
2024-07-30 16:14:40 +0200

    Ajout d'un component enterprise et d'un onglet enterprise dans la navbar du header

--- app-stock/src/app/app.routes.ts
+++ app-stock/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { HomeComponent } from './components/home/home.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EnterpriseComponent } from './components/enterprise/enterprise.component';
 
 export const routes: Routes = [
     {path : "signup",component:SignupComponent},
     {path : "signin",component:SigninComponent},
     {path : "home",component:HomeComponent},
-    {path : "dashboard",component:DashboardComponent}
+    {path : "dashboard",component:DashboardComponent},
+    {path : "enterprise",component:EnterpriseComponent}
 ];
