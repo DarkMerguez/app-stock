53239fce66abbff6d61de0545ed43be0a5f9b64a
DarkMerguez <jlaroche.69008@gmail.com>
2024-09-19 00:16:10 +0200

    ajout du composant product-details pour afficher un seul produit sur la page quand on clique

--- app-stock/src/app/components/product-details/product-details.component.ts
+++ app-stock/src/app/components/product-details/product-details.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { ApiService } from '../../../services/api.service';
+import {MatCardModule} from '@angular/material/card';
+import { Product } from '../../../utils/interfaces/product';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,RouterLink],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product = {} as Product;
+
+  @Input() id: number = 0;
+
+  private api = inject(ApiService);
+
+  ngOnInit(): void {
+    this.api.getProductById(this.id).subscribe((product: Product) => {
+      this.product = product;
+    });
+  }
 
 }
--- app-stock/src/services/api.service.ts
+++ app-stock/src/services/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Products } from '../utils/interfaces/product';
+import { Product, Products } from '../utils/interfaces/product';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -13,6 +13,10 @@ export class ApiService {
 
   public getProducts(): Observable<Products> {
     return this.http.get<Products>("http://localhost:8051/products");
+  }
+
+  public getProductById(id: number): Observable<Product> {
+    return this.http.get<Product>("http://localhost:8051/product/" + id);
   }
 
 }
