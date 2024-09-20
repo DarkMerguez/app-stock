686f4547cc6e2b735ec7a77291a15a793214f08a
DarkMerguez <jlaroche.69008@gmail.com>
2024-09-20 12:40:13 +0200

    Pöursuite de l'implémentation de la logique de connexion et de role

--- app-stock/src/app/components/signin/signin.component.ts
+++ app-stock/src/app/components/signin/signin.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { SignupComponent } from '../signup/signup.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [SignupComponent,RouterLink],
+  imports: [SignupComponent,RouterLink,FormsModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
+
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        // Stockez le token dans le localStorage ou un service d'état
+        localStorage.setItem('token', response.token);
+        // Redirigez l'utilisateur vers la page souhaitée après la connexion
+        this.router.navigate(['/dashboard']); // Ajustez selon votre besoin
+      },
+      error: (error) => {
+        // Gérez l'erreur de connexion (affichez un message, etc.)
+        console.error('Login failed', error);
+      }
+    });
+  }
 
 }
